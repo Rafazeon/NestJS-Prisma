5f71b4b8d3ff03e654150da74b28ec679eb9a19b
Rafael Oliveira <rafazeon@gmail.com>
2023-07-12 02:06:29 -0300

    feat/handle-errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConflictInterceptor } from './common/filters/http-exception/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/filters/http-exception/errors/interceptors/database.interceptor';
+import { NotFoundInterceptor } from './common/filters/http-exception/errors/interceptors/notfound.interceptor';
+import { UnauthorizedInterceptor } from './common/filters/http-exception/errors/interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +15,12 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
   await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // ...
+  watchOptions: {
+    poll: 1000, // Check for changes every second
+    ignored: /node_modules/, // Ignore node_modules directory
+  },
+  // ...
+};
